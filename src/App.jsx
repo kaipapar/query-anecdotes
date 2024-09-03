f90ac4728cec237580399aa86091a714d67ebf26
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-09-03 13:30:45 +0300

    6.20 done
    Dependencies up and running
    gets anecdotes from json server
    gives error message if there is error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 const App = () => {
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+  if (result.isError){
+    return <div>some error!</div>
+  }
 
+  const anecdotes = result.data
+  
   return (
     <div>
       <h3>Anecdote app</h3>
